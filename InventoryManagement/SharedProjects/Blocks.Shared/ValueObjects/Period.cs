2338e8c15ccd3b33b0e8bd38f8a6b60675e133d7
dauda_kad <dbkad@yahoo.co.uk>
2022-06-10 00:19:31 +0100

    Update shared project

---
+++ InventoryManagement/SharedProjects/Blocks.Shared/ValueObjects/Period.cs
@@ -0,0 +1,8 @@
+﻿namespace Blocks.Shared.ValueObjects;
+
+public record Period
+{
+    public DateTimeOffset StartDate { get; set; }
+    public DateTimeOffset EndDate { get; set; }
+    public TimeZoneInfo TimeZoneInfo  { get; set; } = TimeZoneInfo.Utc;
+}
