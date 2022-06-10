7063ae9d13faf066820652c431662013e7344ced
dauda_kad <dbkad@yahoo.co.uk>
2022-06-11 00:53:30 +0100

    Add sales logic

--- InventoryManagement/Sales/Sales.Core/Domain/Sales.cs
+++ InventoryManagement/Sales/Sales.Core/Domain/Sales.cs
@@ -1,4 +1,6 @@
 ﻿using Blocks.Shared.ValueObjects;
+using CSharpFunctionalExtensions;
+using Sales.Core.ValueObjects;
 
 namespace Sales.Core.Domain;
 
@@ -6,5 +8,61 @@ public class Sales
 {
     public EntityName Name { get; set; }
     public Period SalesPeriod { get; set; }
-    public IList<SalesCost> SalesCosts { get; set; }
+    public IList<SalesCost> SalesCosts { get; set; } = new List<SalesCost>();
+
+    private Sales(EntityName name, Period salesPeriod)
+    {
+        Name = name;
+        SalesPeriod = salesPeriod;
+    }
+    
+    public Result RemoveSalesCost(SalesCostType salesCostType)
+    {
+        var existingSalesCost = SalesCosts.SingleOrDefault(s => s.CostType == salesCostType);
+
+        if (existingSalesCost is not null)
+        {
+            SalesCosts.Remove(existingSalesCost);
+        }
+        return Result.Success();
+    }
+    
+    public Result CreateSalesCost(SalesCost salesCost)
+    {
+        if (SalesCosts.Any(s => s.CostType == salesCost.CostType))
+        {
+            return Result.Failure($"There is already an entry with cost type {salesCost.CostType}");
+        }
+
+        SalesCosts.Add(salesCost);
+
+        return Result.Success();
+    }
+    
+    public Result UpdateCostPerUnit(SalesCostType costType, Money amount)
+    {
+        var existingSalesCost = SalesCosts.SingleOrDefault(s => s.CostType == costType);
+        
+        return existingSalesCost is null 
+            ? Result.Failure($"There is no entry with cost type {costType}") 
+            : UpdateSales(amount, money => existingSalesCost.UpdateCostPerUnit(money));
+    }
+
+    public Result UpdateAmount(SalesCostType costType, Money amount)
+    {
+        var existingSalesCost = SalesCosts.SingleOrDefault(s => s.CostType == costType);
+        
+        return existingSalesCost is null 
+            ? Result.Failure($"There is no entry with cost type {costType}") 
+            : UpdateSales(amount, money => existingSalesCost.UpdateAmount(money));
+    }
+
+    private static Result UpdateSales(Money amount, Func<Money, Result> updateSales)
+    {
+        return updateSales(amount);
+    }
+    public static Sales CreateSales(EntityName name, Period salesPeriod)
+    {
+        return new Sales(name, salesPeriod);
+    }
 }
--- InventoryManagement/Sales/Sales.Core/Domain/SalesCost.cs
+++ InventoryManagement/Sales/Sales.Core/Domain/SalesCost.cs
@@ -1,4 +1,5 @@
 ﻿using Blocks.Shared.ValueObjects;
+using CSharpFunctionalExtensions;
 using Sales.Core.ValueObjects;
 
 namespace Sales.Core.Domain;
@@ -8,4 +9,29 @@ public class SalesCost
     public Money Amount { get; set; }
     public Money CostPerUnit { get; set; }
     public SalesCostType CostType { get; set; }
+
+    private SalesCost(Money amount, Money costPerUnit, SalesCostType costType)
+    {
+        Amount = amount;
+        CostPerUnit = costPerUnit;
+        CostType = costType;
+    }
+    
+    public Result UpdateAmount(Money amount)
+    {
+        Amount = amount;
+        
+        return Result.Success();
+    }
+
+    public Result UpdateCostPerUnit(Money costPerUnit)
+    {
+        CostPerUnit = costPerUnit;
+        
+        return Result.Success();
+    }
+    public static SalesCost CreateSalesCost(Money amount, Money costPerUnit, SalesCostType costType)
+    {
+        return new SalesCost(amount, costPerUnit, costType);
+    }
 }
