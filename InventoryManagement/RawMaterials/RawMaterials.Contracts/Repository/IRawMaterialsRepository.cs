e1d7659c2571631bffe2827f7cb3b086e29bfbee
dauda_kad <dbkad@yahoo.co.uk>
2022-06-11 01:00:52 +0100

    Add Raw Material Contracts

---
+++ InventoryManagement/RawMaterials/RawMaterials.Contracts/Repository/IRawMaterialsRepository.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace RawMaterials.Contracts.Repository;
+
+public interface IRawMaterialsRepository<TRawMaterial>
+{
+    IEnumerable<TRawMaterial> GetAllInventoryItems();
+    Result SaveInventoryItem(TRawMaterial inventoryItem);
+    Result DeleteInventory(Guid inventorItemId);
+}
